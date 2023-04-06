069d18038f40cfcb014090ac1b8d4caf2d5afb1d
Veticia <668573+Veticia@users.noreply.github.com>
2023-04-06 04:27:09 +0200

    Ignore fonts

--- Launcher/App/src/main/java/com/veticia/piLauncherNext/SettingsProvider.java
+++ Launcher/App/src/main/java/com/veticia/piLauncherNext/SettingsProvider.java
@@ -115,6 +115,7 @@ public class SettingsProvider
                 if (pkg.startsWith("com.pico.metricstool")) isSystemApp = false;
                 if (pkg.startsWith("com.pvr")) isSystemApp = true;
                 if (pkg.startsWith("com.ss.android.ttvr")) isSystemApp = false;
+                if (pkg.startsWith("com.monotype.android.font")) isSystemApp = true;
                 if (!isSystemApp && !isEnvironment) {
                     if(!installedApplication.packageName.equals(ownPackageName)) {
                         appMap.put(installedApplication.packageName, installedApplication);
